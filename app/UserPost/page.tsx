a294e9b96f277d45e323a847fb088b5147abbcb9
Jakub <jakuboksa@gmail.com>
2023-06-20 01:07:09 +0200

    Add user profile skeleton

--- app/UserPost/page.tsx
+++ app/UserPost/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './UserPost.module.scss';
 
 interface Metadata {
   title: string;
@@ -6,12 +7,46 @@ interface Metadata {
 }
 
 export const metadata: Metadata = {
-  title: 'Journi | Profile ',
+  title: 'Journi | Profile',
   description: 'Your profile page.',
 };
 
 const UserPostPage = () => {
-  return <div>This is only visible for logged in users.</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.profile}>
+        <div className={styles.profilePicture} />
+        <div className={styles.profileInfo}>
+          <div className={styles.username}>John Doe</div>
+          <div className={styles.status}>Active</div>
+        </div>
+      </div>
+      <div className={styles.separator} />
+      <div className={styles.statsContainer}>
+        <h3 className={styles.statisticsHeading}>Traveller Statistics</h3>
+        <div className={styles.stats}>
+          <div className={styles.statCard}>
+            <div className={styles.statHeading}>Hotels Visited</div>
+            <div className={styles.statNumber}>nA</div>
+          </div>
+          <div className={styles.statCard}>
+            <div className={styles.statHeading}>Countries Visited</div>
+            <div className={styles.statNumber}>nA</div>
+          </div>
+        </div>
+        <div className={styles.stats}>
+          <div className={styles.statCard}>
+            <div className={styles.statHeading}>Reviews Written</div>
+            <div className={styles.statNumber}>nA</div>
+          </div>
+          <div className={styles.statCard}>
+            <div className={styles.statHeading}>Stars Given</div>
+            <div className={styles.statNumber}>nA</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserPostPage;
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Home: React.FC = () => {
   return (
-    <section>
-      <div>
-        <h1>Journi</h1>
-        <h2>Your boutique travel community</h2>
-      </div>
-    </section>
+    <main>
+      <h1>Journi</h1>
+      <h2>Your boutique travel community</h2>
+    </main>
   );
 };
 
